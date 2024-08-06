005e542eb5e0e3f35ded8a6f025e25e85aa4d53f
Nofiniaina RANDRIAMBOLA <nofyrnd@gmail.com>
2024-08-06 09:04:27 +0300

    created some utils and components

---
+++ src/utils/Constant.tsx
@@ -0,0 +1,25 @@
+/**
+ * Defines the levels of difficulty for the Minesweeper game.
+ */
+export const LEVELS = {
+    BEGINNER: {
+        ROWS: 10,
+        COLUMNS: 8,
+        BOMBS: 7,
+    },
+    EASY: {
+        ROWS: 14,
+        COLUMNS: 9,
+        BOMBS: 15,
+    },
+    MEDIUM: {
+        ROWS: 20,
+        COLUMNS: 15,
+        BOMBS: 40,
+    },
+    HARD: {
+        ROWS: 26,
+        COLUMNS: 19,
+        BOMBS: 99,
+    },
+};
